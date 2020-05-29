dacd64b482be8c7cfc9e8b40708e488d40779f97
cram <julien.augugliaro@epitech.eu>
2020-05-29 03:15:34 +0200

    Added: scripts, minor changes to games page

---
+++ hide_div.js
@@ -0,0 +1,9 @@
+function hideDiv(id) {
+    var div = document.getElementById(id);
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    }
+    else {
+        div.style.display = "none";
+    }
+}
