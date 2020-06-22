6c4449c5b4254613a8b83102ac6681f7c9adcfd1
Cram0 <julien.augugliaro@epitech.eu>
2020-06-22 10:41:02 +0200

    Trying out pushing using the new distro

--- hide_div.js
+++ hide_div.js
@@ -1,9 +1,13 @@
 function hideDiv(id) {
+
     var div = document.getElementById(id);
+
     if (div.style.display === "none") {
         div.style.display = "block";
     }
+
     else {
         div.style.display = "none";
     }
+
 }
